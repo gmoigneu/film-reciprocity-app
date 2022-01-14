9d6416fb0062cfca9ab0731aa2746d76e86b632f
Guillaume Moigneu <g@moigneu.com>
2022-01-13 20:15:19 -0600

    Initial Commit

---
+++ ReciprocityTimer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ReciprocityTimer
+//
+//  Created by Guillaume Moigneu on 1/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ReciprocityTimer/ReciprocityTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReciprocityTimerApp.swift
+//  ReciprocityTimer
+//
+//  Created by Guillaume Moigneu on 1/13/22.
+//
+
+import SwiftUI
+
+@main
+struct ReciprocityTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
